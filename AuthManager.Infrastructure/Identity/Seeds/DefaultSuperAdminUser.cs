23c981ac5231a4ffadce52cc637fa72d455c7f7c
edmondeh <edmond.hashani@hotmail.com>
2021-07-15 11:56:23 +0200

    Roles and Permissions

---
+++ AuthManager.Infrastructure/Identity/Seeds/DefaultSuperAdminUser.cs
@@ -0,0 +1,60 @@
+﻿using AuthManager.Application.Constants;
+using AuthManager.Application.Enums;
+using AuthManager.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthManager.Infrastructure.Identity.Seeds
+{
+    public static class DefaultSuperAdminUser
+    {
+        public static async Task AddPermissionClaim(this RoleManager<IdentityRole> roleManager, IdentityRole role, string module)
+        {
+            var allClaims = await roleManager.GetClaimsAsync(role);
+            var allPermissions = Permissions.GeneratePermissionsForModule(module);
+            foreach (var permission in allPermissions)
+            {
+                if (!allClaims.Any(a => a.Type == "Permission" && a.Value == permission))
+                {
+                    await roleManager.AddClaimAsync(role, new Claim(CustomClaimTypes.Permission, permission));
+                }
+            }
+        }
+
+        private async static Task SeedClaimsForSuperAdmin(this RoleManager<IdentityRole> roleManager)
+        {
+            var adminRole = await roleManager.FindByNameAsync("SuperAdmin");
+            await roleManager.AddPermissionClaim(adminRole, "Users");
+        }
+
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@gmail.com",
+                FirstName = "Edmond",
+                LastName = "Hashani",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Password1");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.User.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Moderator.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.SuperAdmin.ToString());
+                }
+                await roleManager.SeedClaimsForSuperAdmin();
+            }
+        }
+    }
+}
---
+++ AuthManager.Web/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthManager.Web.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
